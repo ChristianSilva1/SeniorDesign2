8fdfed4dea5c471b9d29d6aa906ab558f4a7d81e
ChristianSilva1 <59715321+ChristianSilva1@users.noreply.github.com>
2022-03-01 15:00:11 -0500

    Setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(5000, console.log("Server started on PORT 5000"));
